65ec2c33026fa1ddc2c8e477fb5c68702230e242
Dev Uppin <devuppin@gmail.com>
2022-11-23 12:42:13 -0800

    made buttin work only once

--- script/scripts.js
+++ script/scripts.js
@@ -2,5 +2,6 @@ const clickMeButton  = document.querySelector(".clickMe")
 // console.log(clickMeButton);
 function clickhandler () {
     alert("You clicked me");
+    // clickMeButton.removeEventListener('click', clickhandler)
 }
-clickMeButton.addEventListener('click', clickhandler)
+clickMeButton.addEventListener('click', clickhandler, {once:true})
